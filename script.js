cf10bc05c54775b351c6b8368db89c780d447483
tanpanamap <memekoocak@gmail.com>
2022-04-27 07:04:49 +0530

    ?????????

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ setInterval(function () {
 }, 10);
 
 function jump() {
-  jumping = 1g;
+  jumping = 1;
   let jumpCount = 0;
   var jumpInterval = setInterval(function () {
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
